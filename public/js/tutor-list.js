26cf7b77e13463b896d7940640015c6cb982aecb
Joseph Ruff <joegruff16@gmail.com>
2024-06-05 18:16:40 -0400

    Removed internal HTML style from cards and applied it to CSS to help with DOM manipulation

---
+++ public/js/tutor-list.js
@@ -0,0 +1,6 @@
+// Create functions that will dynamically generate the HTML on the page
+
+// Function to create container for cards
+// Creates DIV for card, imagecardbody
+
+// Function to create 
