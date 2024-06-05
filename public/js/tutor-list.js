2a002756dea3f7d373dcb1ae6086a45dbe2eb611
Joseph Ruff <joegruff16@gmail.com>
2024-06-05 18:49:39 -0400

    Removed links for social media within cards and began to set variables to select HTML elements for DOM manipulation.

--- public/js/tutor-list.js
+++ public/js/tutor-list.js
@@ -1,6 +1,15 @@
+// Create variables that will pull the desired HTML
+const cardContainer = document.querySelector(".container");
+const card = getElementsByClassName("card mb-2");
+const cardRow = getElementsByClassName("row g-2");
+
+
+
 // Create functions that will dynamically generate the HTML on the page
+const generateCard =
 
 // Function to create container for cards
-// Creates DIV for card, imagecardbody
 
-// Function to create 
+// Creates DIV for card, image on card
+
+// Function to create necessary classes to display card styles
