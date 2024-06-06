31d7cb08353faa26a5edc2ee66f5bd755364f622
Joseph Ruff <joegruff16@gmail.com>
2024-06-05 22:53:54 -0400

    Got the card to display on the ppage dynamically but the style is off. Adjusted the inner HTML styling for columns and need to add style somehow to JS

--- public/js/tutor-list.js
+++ public/js/tutor-list.js
@@ -14,8 +14,10 @@ console.log(cardRowAllCards)
 
 // After search is made card will be created dynamically
 const tutorCard = document.createElement('div');
+tutorCard.setAttribute('class', 'card');
 console.log(tutorCard)
 const cardImage = document.createElement('img');
+cardImage.setAttribute('class', 'col-md-4');
 console.log(cardImage)
 const cardTutorName = document.createElement('h5');
 console.log(cardTutorName)
@@ -25,32 +27,52 @@ const cardTutorLocation = document.createElement('p');
 console.log(cardTutorLocation) // There's a margin and space to edit here
 const cardContactInfo = document.createElement('h6');
 console.log(cardContactInfo)
+const tutorEmail = document.createElement('h6');
+console.log(tutorEmail);
+const tutorPhone = document.createElement('h6');
+console.log(tutorPhone)
 
 
-tutorCard.setAttribute('class', 'card');
-cardImage.setAttribute('class', 'col-md-4');
-card.append(tutorCard);
-console.log(card)
+
+
+// card.append(tutorCard);
+// console.log(card)
 
 // card.append(cardTutorName, cardTutorLocation, tutorInstrument, cardContactInfo);
 // console.log(tutorCard)
 
-// Variable to store empty string so that when a name is inputted it will add the inputted name to the card heading
-const tName = '';
-cardTutorName.textContent = `${tName}`;
-console.log(cardTutorName)
+// Variables to store empty string so that when data is entered by the user in the registration it will populate on the card
+const tName = 'Tutor Name';
+cardTutorName.textContent = tName;
+// cardTutorName.setAttribute()
+console.log(cardTutorName);
 
-const tInstrument = '';
-tutorInstrument.textContent = `${tInstrument}`;
+const tInstrument = 'Instrument';
+tutorInstrument.textContent = tInstrument;
 console.log(tutorInstrument);
+
+const tLocation = 'Location';
+cardTutorLocation.textContent = tLocation;
+console.log(cardTutorLocation);
+
+const cInfo = 'Contact Info';
+cardContactInfo.textContent = cInfo;
+console.log(cardContactInfo);
+
+const tEmail = 'Email:';
+tutorEmail.textContent = tEmail;
+console.log(tutorEmail);
+
+const tPhone = 'Phone:';
+tutorPhone.textContent = tPhone;
+console.log(tutorPhone);
+
+// Append elements to the tutorCard
+tutorCard.append(cardImage, cardTutorName, tutorInstrument, cardTutorLocation, cardContactInfo, tutorEmail, tutorPhone);
+
+// Append the tutorCard to the container
+cardContainer.append(tutorCard);
+
+// Adjust classes and styles so the card will display correctly on the page
+
 // Clear contents of the card
-
-
-// Create functions that will dynamically generate the HTML on the page
-// const generateCard =
-
-// Function to create container for cards
-
-// Creates DIV for card, image on card
-
-// Function to create necessary classes to display card styles
