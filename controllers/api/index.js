bd4b558e9a69a7b5d6b7a7fa65300f5c2ee101c2
David Rauf <davidrauf@gmail.com>
2024-06-06 20:55:51 -0400

    update

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const tutorRoutes = require('./tutorRoutes');
 
 router.use('/users', userRoutes);
+router.use('/tutors', tutorRoutes);
 
 module.exports = router;
