dce87b31612d3bc87e6385d157e26f7678cba0a5
Joseph Ruff <joegruff16@gmail.com>
2024-06-07 23:04:30 -0400

    Found bug in index.js model in through option for join. Seeds still won't populate. A weird dialect error.

--- models/index.js
+++ models/index.js
@@ -1,94 +1,92 @@
-const User = require('./User');
-const Tutor = require('./Tutor');
-const Student = require('./Student');
-const Certifications = require('./Certifications');
-const Instrument = require('./Instrument');
-const Specialties = require('./Specialties');
-const StudentFavorites = require('./StudentFavorites');
-const TutorCertifications = require('./TutorCertifications');
-const TutorComments = require('./TutorComments');
-const TutorInstrument = require('./TutorInstrument');
-const TutorSocialLinks = require('./TutorSocialLinks');
-const TutorSpecialties = require('./TutorSpecialties');
- 
-
+const User = require("./User");
+const Tutor = require("./Tutor");
+const Student = require("./Student");
+const Certifications = require("./Certifications");
+const Instrument = require("./Instrument");
+const Specialties = require("./Specialties");
+const StudentFavorites = require("./StudentFavorites");
+const TutorCertifications = require("./TutorCertifications");
+const TutorComments = require("./TutorComments");
+const TutorInstrument = require("./TutorInstrument");
+const TutorSocialLinks = require("./TutorSocialLinks");
+const TutorSpecialties = require("./TutorSpecialties");
 
 // Tutors belongsTo User
 Tutor.belongsTo(User, {
-    foreignKey: 'user_id',
+  foreignKey: "user_id",
 });
 
 // Students belongsTo user
 Student.belongsTo(User, {
-    foreignKey: 'user_id',
+  foreignKey: "user_id",
 });
 
 // Tutors have many instruments
 Tutor.hasMany(Instrument, {
-    foreignKey: 'instrument_id',
+  foreignKey: "instrument_id",
 });
 
 // Instruments belongToMany Tutors (through TutorInstruments)
 Instrument.belongsToMany(Tutor, {
-    through: TutorInstrument,
-    foreignKey: 'instrument_id'
+  through: TutorInstrument,
+  foreignKey: "instrument_id",
 });
 
-// Tutors belongToMany Instruments (through TutorInstruments) 
+// Tutors belongToMany Instruments (through TutorInstruments)
 Tutor.belongsToMany(Instrument, {
-    through: TutorInstrument,
-    foreignKey: 'tutor_id',
+  through: TutorInstrument,
+  foreignKey: "tutor_id",
 });
 
-// Tutors has many Social links 
+// Tutors has many Social links
 Tutor.hasMany(TutorSocialLinks, {
-    foreignKey: 'tutor_id',
+  foreignKey: "tutor_id",
 });
 
 // Tutors belongToMany specialties (throught TutorSpecialties)
 Tutor.belongsToMany(Specialties, {
-    through: TutorSpecialties,
-    foreignKey: 'tutor_id',
+  through: TutorSpecialties,
+  foreignKey: "tutor_id",
 });
 
 // specialties belong to many tutors (throught TutorSpecialties)
 Specialties.belongsToMany(Tutor, {
-    through: TutorSpecialties,
-    foreignKey: 'specialty_id'
+  through: TutorSpecialties,
+  foreignKey: "specialty_id",
 });
 
 // Tutors belongToMany Certifications (through TutorCertifications)
 Tutor.belongsToMany(Certifications, {
-    through: TutorCertifications,
-    foreignKey: 'tutor_id',
+  through: TutorCertifications,
+  foreignKey: "tutor_id",
 });
 
 // Certifications belongToMany Tutors (throught TutorCertifications)
 Certifications.belongsToMany(Tutor, {
-    throught: TutorCertifications,
-    foreignKey: 'certification_id'
-})
+  through: TutorCertifications,
+  foreignKey: "certification_id",
+});
 
 // Tutors have many Comments
 Tutor.hasMany(TutorComments, {
-    foreignKey: 'tutor_id'
+  foreignKey: "tutor_id",
 });
 // Students have many Favorites
 Student.hasMany(StudentFavorites, {
-    foreignKey: 'tutor_id'
+  foreignKey: "tutor_id",
 });
 
-module.exports = { 
-    User,
-    Tutor,
-    Student,
-    Certifications,
-    Instrument,
-    Specialties,
-    StudentFavorites,
-    TutorCertifications,
-    TutorComments,
-    TutorInstrument,
-    TutorSocialLinks,
-    TutorSpecialties, 
+module.exports = {
+  User,
+  Tutor,
+  Student,
+  Certifications,
+  Instrument,
+  Specialties,
+  StudentFavorites,
+  TutorCertifications,
+  TutorComments,
+  TutorInstrument,
+  TutorSocialLinks,
+  TutorSpecialties,
 };
