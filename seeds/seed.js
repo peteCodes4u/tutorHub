bd0332ed1a546589eb748eb79737ce659550646e
Joseph Ruff <joegruff16@gmail.com>
2024-06-08 11:09:53 -0400

    Working on completing seeds js

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,15 +2,42 @@ require("dotenv").config({
   path: "../.env",
 });
 const sequelize = require("../config/connection");
-const { User } = require("../models");
-const { Tutor } = require("../models");
+const {
+  Certifications,
+  Instrument,
+  Specialties,
+  Student,
+  StudentFavorites,
+  Tutor,
+  TutorCertifications,
+  TutorComments,
+  TutorInstrument,
+  TutorSocialLinks,
+  TutorSpecialties,
+  User,
+} = require("../models");
 
 const userData = require("./userData.json");
 const tutorData = require("./tutorData.json");
+const instrumentsData = require("./instrumentData.json");
+const certsData = require("./certificationsData.json");
+const certsData = require("./certificationsData.json");
+const certsData = require("./certificationsData.json");
+const certsData = require("./certificationsData.json");
 
 const seedDatabase = async () => {
   try {
     await sequelize.sync({ force: true });
+
+    await Instrument.bulkCreate(instrumentsData, {
+      individualHooks: true,
+      returning: true,
+    });
+
+    await Certifications.bulkCreate(certsData, {
+      individualHooks: true,
+      returning: true,
+    });
 
     await User.bulkCreate(userData, {
       individualHooks: true,
