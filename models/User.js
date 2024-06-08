501abb195b9465c4e375df0e3f0ba180f99072d1
Joseph Ruff <joegruff16@gmail.com>
2024-06-08 09:01:16 -0400

    Found many bugs and one of the reasons why I can't run the seed is because for some reason I only have two created tables User and Sessions and it won't let me add the remaining tables. There's several other errors and some bugs that I fixed that included misspelled tutors and through.

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
-const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
 
 class User extends Model {
   checkPassword(loginPw) {
@@ -19,18 +19,18 @@ User.init(
     firstName: {
       type: DataTypes.STRING,
       allowNull: false,
-      primaryKey: true
+      primaryKey: false,
     },
     lastName: {
       type: DataTypes.STRING,
       allowNull: false,
-      primaryKey: true
+      primaryKey: false,
     },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
-      primaryKey: true,
+      primaryKey: false,
       validate: {
         isEmail: true,
       },
@@ -45,7 +45,7 @@ User.init(
     userRole: {
       type: DataTypes.STRING,
       allowNull: false,
-    }
+    },
   },
   {
     hooks: {
@@ -58,7 +58,7 @@ User.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'user',
+    modelName: "user",
   }
 );
 
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,27 +1,32 @@
 require("dotenv").config({
-  path: '../.env'
-})
-const sequelize = require('../config/connection');
-const { User, Instrument } = require('../models');
-const { Tutor } = require('../models');
+  path: "../.env",
+});
+const sequelize = require("../config/connection");
+const { User } = require("../models");
+const { Tutor } = require("../models");
 
-const userData = require('./userData.json');
-const tutorData = require('./tutorData.json');
+const userData = require("./userData.json");
+const tutorData = require("./tutorData.json");
 
 const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
+  try {
+    await sequelize.sync({ force: true });
 
-  await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
-  });
+    await User.bulkCreate(userData, {
+      individualHooks: true,
+      returning: true,
+    });
 
-  await Tutor.bulkCreate(tutorData, {
-    individualHooks: true,
-    returning: true,
-  });
+    await Tutor.bulkCreate(tutorData, {
+      individualHooks: true,
+      returning: true,
+    });
 
-  process.exit(0);
+    process.exit(0);
+  } catch (error) {
+    console.error("Error in this data", error);
+    process.exit(1);
+  }
 };
 
 seedDatabase();
