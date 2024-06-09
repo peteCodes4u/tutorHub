bba275c6e1a4f4f8d8d65f9be3a40fbbbbc5b2df
Joseph Ruff <joegruff16@gmail.com>
2024-06-09 17:24:06 -0400

    Updated index.js api to create variables that require the routes and to implement router.use to use the routes. Added TutorPrices model. Working on adding all additional apis that I created routes for

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,15 +1,33 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const registrationRoutes = require('./registrationRoutes');
-const tutorRegistrationRoutes = require('./tutorRegistrationRoutes');
-const tutorRoutes = require('./tutorRoutes');
-const instrumentRoutes = require('./instrumentRoutes');
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
 
+// Created variables to require the routes
+const registrationRoutes = require("./registrationRoutes");
+const tutorRegistrationRoutes = require("./tutorRegistrationRoutes");
+const tutorRoutes = require("./tutorRoutes");
+const instrumentRoutes = require("./instrumentRoutes");
+const specialtiesRoutes = require("./specialtiesRoutes");
+const studentRoutes = require("./studentRoutes");
+const studentFavoritesRoutes = require("./studentFavoritesRoutes");
+const tutorCertificationsRoutes = require("./tutorCertificationsRoutes");
+const tutorCommentsRoutes = require("./tutorCommentsRoutes");
+const tutorInstrumentsRoutes = require("./tutorInstrumentsRoutes");
+const tutorSocialLinksRoutes = require("./tutorSocialLinksRoutes");
+const tutorSpecialtiesRoutes = require("./tutorSpecialtiesRoutes");
 
-router.use('/users', userRoutes);
-router.use('/registration', registrationRoutes);
-router.use('/tutorRegistration', tutorRegistrationRoutes);
-router.use('/tutors', tutorRoutes);
-router.use('/instruments', instrumentRoutes);
+// Used router.use to use the routes
+router.use("/users", userRoutes);
+router.use("/registration", registrationRoutes);
+router.use("/tutorRegistration", tutorRegistrationRoutes);
+router.use("/tutors", tutorRoutes);
+router.use("/instruments", instrumentRoutes);
+router.use("/specialties", specialtiesRoutes);
+router.use("/students", studentRoutes);
+router.use("/studentFavorites", studentFavoritesRoutes);
+router.use("/tutorCertifications", tutorCertificationsRoutes);
+router.use("/tutorComments", tutorCommentsRoutes);
+router.use("/tutorInstruments", tutorInstrumentsRoutes);
+router.use("/tutorSocialLinks", tutorSocialLinksRoutes);
+router.use("/tutorSpecialties", tutorSpecialtiesRoutes);
 
 module.exports = router;
